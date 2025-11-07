da8f4a7dfb0a4bc6d784ddc9ccdeb771cf6671aa
Bahrom1dd1n <komiljonovbahromiddin@gmail.com>
2025-11-07 23:38:32 +0500

    added vfd_controller source and header files

---
+++ Core/Inc/vfd_controller.h
@@ -0,0 +1,15 @@
+/*
+ * vfd_controller.h
+ *
+ *  Created on: Nov 7, 2025
+ *      Author: bahromiddin
+ */
+
+#ifndef INC_VFD_CONTROLLER_H_
+#define INC_VFD_CONTROLLER_H_
+#include <stdint.h>
+
+
+
+
+#endif /* INC_VFD_CONTROLLER_H_ */
---
+++ Core/Src/vfd_controller.c
@@ -0,0 +1,8 @@
+/*
+ * vfd_controller.c
+ *
+ *  Created on: Nov 7, 2025
+ *      Author: bahromiddin
+ */
+
+#include "vfd_controller.h"
